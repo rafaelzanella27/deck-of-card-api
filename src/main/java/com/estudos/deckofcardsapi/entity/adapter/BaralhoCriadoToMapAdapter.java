5436abdf5badc2dbbd64baeb2f562c1103b5de30
razrodrigues <rafael.rodrigues@klab.com.br>
2022-03-27 18:24:57 -0300

    Implementando funções

--- src/main/java/com/estudos/deckofcardsapi/entity/adapter/BaralhoCriadoToMapAdapter.java
+++ src/main/java/com/estudos/deckofcardsapi/entity/adapter/BaralhoCriadoToMapAdapter.java
@@ -1,19 +1,34 @@
 package com.estudos.deckofcardsapi.entity.adapter;
 
 import com.estudos.deckofcardsapi.domain.BaralhoDomain;
+import com.estudos.deckofcardsapi.domain.CartaDomain;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class BaralhoCriadoToMapAdapter {
 
 
     public Map<String, Object> execute(BaralhoDomain baralhoDomain){
         Map<String, Object> objectMap = new HashMap<>();
-        objectMap.put("sucesso", booleanToString(baralhoDomain.getSucesso()));
-        objectMap.put("embaralhado", booleanToString(baralhoDomain.getEmbaralhado()));
-        objectMap.put("baralho_id", baralhoDomain.getBaralho_id());
-        objectMap.put("restante", baralhoDomain.getRestante());
+        List<CartaDomain> cartaDomains = baralhoDomain.getCartaDomains();
+        if (Objects.nonNull(baralhoDomain.getSucesso())){
+            objectMap.put("sucesso", booleanToString(baralhoDomain.getSucesso()));
+        }
+        if (Objects.nonNull(baralhoDomain.getEmbaralhado())){
+            objectMap.put("embaralhado", booleanToString(baralhoDomain.getEmbaralhado()));
+        }
+        if (Objects.nonNull(baralhoDomain.getBaralho_id())){
+            objectMap.put("baralho_id", baralhoDomain.getBaralho_id());
+        }
+        if (Objects.nonNull(baralhoDomain.getRestante())){
+            objectMap.put("restante", baralhoDomain.getRestante());
+        }
+        if (Objects.nonNull(baralhoDomain.getCartaDomains())){
+            objectMap.put("cartas", cartaDomains);
+        }
         return objectMap;
     }
 
--- src/main/java/com/estudos/deckofcardsapi/entity/adapter/DeckToBaralhoDomainAdapter.java
+++ src/main/java/com/estudos/deckofcardsapi/entity/adapter/DeckToBaralhoDomainAdapter.java
@@ -1,7 +1,9 @@
 package com.estudos.deckofcardsapi.entity.adapter;
 
 import com.estudos.deckofcardsapi.domain.BaralhoDomain;
+import com.estudos.deckofcardsapi.domain.CartaDomain;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -10,10 +12,23 @@ public class DeckToBaralhoDomainAdapter {
     public BaralhoDomain converte(Map<String, Object> entrada){
         BaralhoDomain baralhoDomain = new BaralhoDomain();
         if (Objects.nonNull(entrada)){
-            baralhoDomain.setBaralho_id(entrada.get("deck_id").toString());
-            baralhoDomain.setSucesso(Boolean.valueOf((Boolean) entrada.get("success")));
-            baralhoDomain.setEmbaralhado(Boolean.valueOf((Boolean) entrada.get("shuffled")));
-            baralhoDomain.setRestante(entrada.get("remaining").toString());
+
+            if (entrada.containsKey("deck_id") && Objects.nonNull(entrada.get("deck_id"))){
+                baralhoDomain.setBaralho_id(entrada.get("deck_id").toString());
+            }
+            if (entrada.containsKey("success") && Objects.nonNull(entrada.get("success"))){
+                baralhoDomain.setSucesso(Boolean.valueOf((Boolean) entrada.get("success")));
+            }
+            if (entrada.containsKey("shuffled") && Objects.nonNull(entrada.get("shuffled"))){
+                baralhoDomain.setEmbaralhado(Boolean.valueOf((Boolean) entrada.get("shuffled")));
+            }
+            if (entrada.containsKey("remaining") && Objects.nonNull(entrada.get("remaining"))){
+                baralhoDomain.setRestante(entrada.get("remaining").toString());
+            }
+            if (entrada.containsKey("cards")){
+                List<CartaDomain> cartaDomains = (List<CartaDomain>) entrada.get("cards");
+                baralhoDomain.setCartaDomains(cartaDomains);
+            }
         }
         return baralhoDomain;
     }
